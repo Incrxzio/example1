31f232c32ce320ea970043343032999fa456e74a
Gagebundit Nuntipin <notepoonyapat@gmail.com>
2022-11-07 15:02:45 +0700

    เพิ่มข้อ 2 และ 3

---
+++ index.js
@@ -0,0 +1,35 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'todolist'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
+connection.query('SELECT * FROM todo', function (error, results, fields) {
+    if (error) throw error;
+    console.log('ผลลัพธ์: ', results);
+  });
+
+connection.query('INSERT INTO todo VALUES ("", "ซื้อผัก", 1 , false, null, null)', function (error, results, fields) {
+    if (error) throw error;
+    console.log('ผลลัพธ์: ', results);
+  });
+ 
+  connection.query('UPDATE todo SET todo_name = "พายายไปสวนสนุก" WHERE todo_id = 2', function (error, results, fields) {
+    if (error) throw error;
+    console.log('ผลลัพธ์: ', results);
+  });
+
+  connection.query('DELETE FROM todo WHERE todo_id = 7', function (error, results, fields) {
+    if (error) throw error;
+    console.log('ผลลัพธ์: ', results);
+  });
+connection.end();
